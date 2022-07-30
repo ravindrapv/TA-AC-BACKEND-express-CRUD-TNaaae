56a8013a22294b89ff2678d4e5332e418efe45f4
ravindrapv <ravindra.ravi4849@gmail.com>
2022-07-30 14:17:58 +0530

    update

--- block-BNaacv/user-diary/app.js
+++ block-BNaacv/user-diary/app.js
@@ -15,8 +15,9 @@ app.get('/',(req,res) => {
 
 app.get('/users',usersData)
 
-app.use('/',(req,res) => {
+app.use((req,res,next) => {
     res.status(404).send("page not found");
+    next()
 });
 
 app.listen(3000,() => {
