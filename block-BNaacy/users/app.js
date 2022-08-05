ae2987bd4a65c5c5d78f2d6ce3f8a2f0f3e49b08
ravindrapv <ravindra.ravi4849@gmail.com>
2022-08-05 14:30:29 +0530

    add on

--- block-BNaacy/users/app.js
+++ block-BNaacy/users/app.js
@@ -1,24 +1,27 @@
-var express = require('express');
- var app = express();
-var mongoose = require('mongoose');
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var path = require("path");
 
+var indexRouter = require("./routes/index");
 
-mongoose.connect('mongodb://localhost/usersData',{useNewparser:true,useUniedTopologe:true},(err) => {
-    console.log("connected",err ? false : true);
+mongoose.connect(
+  "mongodb://localhost/usersData",
+  (err) => {
+    console.log("connected", err ? false : true);
+  }
+);
+
+app.set("view engine", "ejs");
+app.set("view", path.join(__dirname, "view"));
+
+//routing miidelwers
+app.use("/", indexRouter);
+//error handeler
+
+app.use((req, res, next) => {
+  res.send("page  not found");
 });
-
-app.get('./',(req,res) => {
-    res.send("welcome")
-})
-app.use((req,res,next) => {
-    res.send('page not found')
-})
-app.listen(3000,() => {
-    console.log("server is listing on port 3000");
-})
-
-
-
-
-
-
+app.listen(3000, () => {
+  console.log("server is listing on port 3000");
+});
