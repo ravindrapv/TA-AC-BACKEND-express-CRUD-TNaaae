16b3bed5e6bb3b98f2caa14fcfd31b1cd3d12884
ravindrapv <ravindra.ravi4849@gmail.com>
2022-08-08 14:24:28 +0530

    fatch the users

--- block-BNaada/users/app.js
+++ block-BNaada/users/app.js
@@ -34,6 +34,6 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.send(err);
 });
-app.listen(3000, () => {
-  console.log("server is listing on port 3000");
+app.listen(8000, () => {
+  console.log("server is listing on port 8000");
 });
